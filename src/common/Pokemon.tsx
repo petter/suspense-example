c9bd3f8ad06f79fa429ae56e918e71d9e9f8064f
Petter Sæther Moen <pettersmoen@gmail.com>
2022-07-18 16:09:38 +0200

    Style pokemons

--- src/common/Pokemon.tsx
+++ src/common/Pokemon.tsx
@@ -1,14 +1,15 @@
 import type { PokemonResponse } from "./PokemonResponse";
+import styles from './Pokemon.module.css';
 
 interface PokemonProps {
   data: PokemonResponse;
 }
 
 export const Pokemon = ({data}: PokemonProps) => {
-  return (
-    <div>
-      <img src={data.sprites.front_default} alt="" />
-      <p>{data.name}</p>
-    </div>
-  )
+    return (
+        <div className={styles.Pokemon}>
+            <h4 className={styles.PokemonName}>{data.name}</h4>
+            <img className={styles.PokemonImage} src={data.sprites.front_default} alt=""/>
+        </div>
+    )
 }
